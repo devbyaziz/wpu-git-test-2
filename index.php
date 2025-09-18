8dc97d96c81f5dc1444947078766432f38ee559a
devbyaziz <azizhabibie2002@gmail.com>
2025-09-18 10:38:57 +0700

    menambahkan list 5

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
         <li>Feature 1</li>
         <li>Feature 2</li>
         <li>Feature 3</li>
+        <li>Feature 4</li>
+        <li>Feature 5</li>
+        <li>Feature 6</li>
     </ul>    
 </body>
 </html>
