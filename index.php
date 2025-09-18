cc512a80fe04dd9482e9c7bdea51ba3ff7c64c5e
devbyaziz <azizhabibie2002@gmail.com>
2025-09-18 10:29:07 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>Hello, World!</h1>
     <p>This is a simple PHP file to test Git remote repository functionality.</p>
     <p>Updated on 2024-06-10 to test commit changes.</p>
+    <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+        <li>Feature 3</li>
+    </ul>    
 </body>
 </html>
